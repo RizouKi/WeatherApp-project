5f590afdbf0c9809e2f43f8c12332d9f8725c89e
RizouKi <olivia.pradervand@gmail.com>
2024-01-23 21:23:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -31,7 +31,6 @@ function displayWeather(response) {
   weatherIconElement.className = weatherIconName;
   weatherIconElement.src = weatherIconURL;
 }
-
 function searchCity(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector(".city-input");
